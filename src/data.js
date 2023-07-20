d8a29a48489831ff0b610dfa9930a7a3a8c5006c
ksuklondon <staniszewskakalina@gmail.com>
2023-07-20 22:46:54 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,90 @@
+export const shortList = [
+  {
+    id: 1,
+    image: "./images/1.jpg",
+    name: "kate olive",
+    title: "senior partner",
+    quote: "Never hate your enemies. It affects your judgment.",
+  },
+];
+
+export const list = [
+  {
+    id: 1,
+    image: "./images/1.jpg",
+    name: "kate olive",
+    title: "senior partner",
+    quote: "Never hate your enemies. It affects your judgment.",
+  },
+  {
+    id: 2,
+    image: "./images/2.jpg",
+    name: "dominic shipman",
+    title: "senior associate",
+    quote:
+      "I chose to go to law school because I thought that someday, somehow I’d make a difference.",
+  },
+  {
+    id: 3,
+    image: "./images/3.jpg",
+    name: "peter smith",
+    title: "solicitor",
+    quote: "A lawyer without books would be like a workman without tools.",
+  },
+  {
+    id: 4,
+    image: "./images/4.jpg",
+    name: "rob green",
+    title: "international law consultant",
+    quote:
+      " Success usually comes to those who are too busy to be looking for it. ",
+  },
+];
+
+export const longList = [
+  {
+    id: 1,
+    image: "./images/1.jpg",
+    name: "kate olive",
+    title: "senior partner",
+    quote: "Never hate your enemies. It affects your judgment.",
+  },
+  {
+    id: 2,
+    image: "./images/2.jpg",
+    name: "dominic shipman",
+    title: "senior associate",
+    quote:
+      "I chose to go to law school because I thought that someday, somehow I’d make a difference.",
+  },
+  {
+    id: 3,
+    image: "./images/3.jpg",
+    name: "peter smith",
+    title: "solicitor",
+    quote: "A lawyer without books would be like a workman without tools.",
+  },
+  {
+    id: 4,
+    image: "./images/4.jpg",
+    name: "rob green",
+    title: "international law consultant",
+    quote:
+      " Success usually comes to those who are too busy to be looking for it. ",
+  },
+  {
+    id: 5,
+    image: "./images/5.jpg",
+    name: "maria larsen",
+    title: "corporate lawyer",
+    quote:
+      "As a lawyer, I can assure you that a lot of document drafting is repetitive, involving cutting and pasting from templates. But the best lawyers bring a unique perspective to the process and anticipate clients’ problems.",
+  },
+  {
+    id: 6,
+    image: "./images/6.jpg",
+    name: "stephanie peake",
+    title: "office manager",
+    quote: "Necessity knows no law; I know some attorneys of the same.",
+  },
+];
